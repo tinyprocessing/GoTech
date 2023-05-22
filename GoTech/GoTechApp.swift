8604d77809d99470064ecbd24c70a6fef5b950f5
tinyprocessing <safir@thenoco.co>
2023-05-22 18:42:20 +0300

    Initial Commit

---
+++ GoTech/GoTechApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoTechApp.swift
+//  GoTech
+//
+//  Created by Michael Safir on 22.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct GoTechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
